be0be58ec7ee151f6d88ced29af429e5876d53c1
wjdekdns1028 <wjdekdns0218@naver.com>
2025-02-22 11:53:07 +0900

    Add changePassword

--- backend/src/main/java/core/backend/controller/UserController.java
+++ backend/src/main/java/core/backend/controller/UserController.java
@@ -1,10 +1,16 @@
 package core.backend.controller;
 
+import core.backend.dto.PasswordChangeRequest;
 import core.backend.dto.UserProfileUpdateRequest;
+import core.backend.exception.CustomException;
+import core.backend.exception.ErrorCode;
+import core.backend.jwt.JwtUtil;
 import core.backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import core.backend.domain.Member;
@@ -19,6 +25,8 @@ import java.util.Map;
 public class UserController {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     //현재 로그인한 사용자 프로필 조회
     @GetMapping("/me")
@@ -49,6 +57,34 @@ public class UserController {
         Map<String, String> response = new HashMap<>();
         response.put("message", "프로필 수정 완료");
         return response;
+    }
+
+    // 비밀번호 수정
+    @PutMapping("/password")
+    public ResponseEntity<String> changePassword(
+            @RequestHeader("Authorization") String token,
+            @RequestBody PasswordChangeRequest request){
+
+        //jwt애서 이메일 추출
+        String email = jwtUtil.extractEmail(token.replace("Bearer", ""));
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        //현재 비밀번호 확인
+        if(!passwordEncoder.matches(request.getCurrentPassword(), member.getPassword())){
+            throw new CustomException(ErrorCode.INVALID_PASSWORD);
+        }
+
+        //새 비밀번호 같은지 검사
+        if(!request.getNewPassword().equals(request.getConfirmPassword())){
+            throw new CustomException(ErrorCode.PASSWORD_NOT_MATCH);
+        }
+
+        //비밀번호 변경, 저장
+        member.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        memberRepository.save(member);
+
+        return ResponseEntity.ok("비밀번호가 성공적으로 변경되었습니다.");
     }
 
     
