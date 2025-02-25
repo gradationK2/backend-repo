6fc4f8db1b6bc7157f51c03889c93a14aa3d3ca5
MINWOO KIM <sunnybikers@naver.com>
2025-02-25 16:14:59 +0900

    ~ 업로드: 이전 커밋 이어서

--- backend/src/main/java/core/backend/domain/BadgeType.java
+++ backend/src/main/java/core/backend/domain/BadgeType.java
@@ -15,12 +15,7 @@ public enum BadgeType {
     REVIEW_15("후기15개", 15, "/images/badges/badge_15.png"),
     REVIEW_20("후기20개", 20, "/images/badges/badge_20.png"),
     REVIEW_30("후기30개", 30, "/images/badges/badge_30.png"),
-    REVIEW_40("후기40개", 40, "/images/badges/badge_40.png"),
-    REVIEW_50("후기50개", 50, "/images/badges/badge_50.png"),
-    REVIEW_60("후기60개", 60, "/images/badges/badge_60.png"),
-    REVIEW_70("후기70개", 70, "/images/badges/badge_70.png"),
-    REVIEW_80("후기80개", 80, "/images/badges/badge_80.png"),
-    REVIEW_150("후기150개", 150, "/images/badges/badge_150.png");
+    REVIEW_50("후기50개", 50, "/images/badges/badge_50.png");
 
     private final String label;
     private final int reviewCount;
@@ -72,18 +67,3 @@ public enum BadgeType {
         return badgeCount;
     }
 }
-
-/**
- * 기준 배찌 모양 예시
- * 후기 1개 올리면 B 배찌(비기너)
- * 후기 10개 올리면 조미료모양 배찌
- * 후기 15개 올리면 K 조각 1 배찌
- * 후기 20개 올리면 K 조각 2배찌
- * 후기 30개 올리면 음식점(모형) 배찌
- * 후기 40개 올리면 K 조각 3 배찌
- * 후기 50개 올리면 전문가 배찌
- * 후기 60개 올리면 K 조각 4 배찌
- * 후기 70개 올리면 K 조각 5배찌
- * 후기 80개 올리면 K 조각 6배찌
- * 후기 150개 올리면 우리 서버 아이콘배찌
- */
